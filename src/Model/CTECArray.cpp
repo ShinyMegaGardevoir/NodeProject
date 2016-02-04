fcaa4936e0bf0f1934fabf1a56eb47823f0531cf
ShinyMegaGardevoir <sadliergirls@gmail.com>
2016-02-04 09:24:36 -0700

    Constructor and Head
    Initialized constructor. The head moves to the front of the current
    array until it is done.

--- src/Model/CTECArray.cpp
+++ src/Model/CTECArray.cpp
@@ -21,6 +21,23 @@ CTECArray<Type>::CTECArray(int size)
 		cerr << "Size must be greater than or equal to 1." << endl;
 		return;
 	}
+
+	for(int index = 0; index < size; index++)
+	{
+		if(head != nullptr)
+		{	//We have more than one ArrayNode
+			ArrayNode<Type> nextNode;
+			nextNode.setNext(head);
+			head = &nextNode;
+
+		}
+		else
+		{	//This is the first node in the array.
+			ArrayNode<Type> first;
+			head = &first;
+
+		}
+	}
 }
 
 template <class Type>
